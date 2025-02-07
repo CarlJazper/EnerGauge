043fb7c235f02f8c01b84f5f09c4935350f533d4
CarlJazper <perezcarljazper+github@gmail.com>
2025-02-07 23:00:03 +0800

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
 import Header from "./components/layouts/Header";
 
-const PrivateRoute = ({ element }) => {
-  return localStorage.getItem('token') ? element : <Navigate to="/login" />;
-};
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<PrivateRoute element={<Home />} />} />
+        <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
